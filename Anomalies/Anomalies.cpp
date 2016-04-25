6fae79dc2a0dd7c88353a7db45c8480b5675edc4
rensso mora <soulchicano@gmail.com>
2016-04-25 16:52:38 -0300

    Begins!!

---
+++ Anomalies/Anomalies.cpp
@@ -0,0 +1,17 @@
+// Anomalies.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+
+#include <opencv2/core/core.hpp>
+#include <opencv2/imgcodecs.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include <iostream>
+#include <string>
+
+int main()
+{
+
+  return 0;
+}
+
