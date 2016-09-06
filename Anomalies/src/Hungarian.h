c64d275772efc5ef2daf5ff0cf2a44a4c2639ad2
MatheusToledo <matheustlandrade@gmail.com>
2016-09-06 14:02:16 -0300

    Previous to Merge

---
+++ Anomalies/src/Hungarian.h
@@ -0,0 +1,665 @@
+
+#ifndef HUNGARIAN_H
+#define HUNGARIAN_H
+
+#include <initializer_list>
+#include <cstdlib>
+#include <ostream>
+#include <iostream>
+#include <ctime>
+#include <cassert>
+#include <algorithm>
+#include <list>
+#include <utility>
+#include <cmath>
+#include <limits>
+
+template <class T>
+class Matrix {
+public:
+  Matrix() {
+    m_rows = 0;
+    m_columns = 0;
+    m_matrix = nullptr;
+  }
+  Matrix(const size_t rows, const size_t columns) {
+    m_matrix = nullptr;
+    resize(rows, columns);
+  }
+  Matrix(const std::initializer_list<std::initializer_list<T>> init) {
+    m_matrix = nullptr;
+    m_rows = init.size();
+    if (m_rows == 0) {
+      m_columns = 0;
+    }
+    else {
+      m_columns = init.begin()->size();
+      if (m_columns > 0) {
+        resize(m_rows, m_columns);
+      }
+    }
+
+    size_t i = 0, j;
+    for (auto row = init.begin(); row != init.end(); ++row, ++i) {
+      assert(row->size() == m_columns && "All rows must have the same number of columns.");
+      j = 0;
+      for (auto value = row->begin(); value != row->end(); ++value, ++j) {
+        m_matrix[i][j] = *value;
+      }
+    }
+  }
+  Matrix(const Matrix<T> &other) {
+    if (other.m_matrix != nullptr) {
+      // copy arrays
+      m_matrix = nullptr;
+      resize(other.m_rows, other.m_columns);
+      for (size_t i = 0; i < m_rows; i++) {
+        for (size_t j = 0; j < m_columns; j++) {
+          m_matrix[i][j] = other.m_matrix[i][j];
+        }
+      }
+    }
+    else {
+      m_matrix = nullptr;
+      m_rows = 0;
+      m_columns = 0;
+    }
+  }
+  Matrix<T> & operator= (const Matrix<T> &other) {
+    if (other.m_matrix != nullptr) {
+      // copy arrays
+      resize(other.m_rows, other.m_columns);
+      for (size_t i = 0; i < m_rows; i++) {
+        for (size_t j = 0; j < m_columns; j++) {
+          m_matrix[i][j] = other.m_matrix[i][j];
+        }
+      }
+    }
+    else {
+      // free arrays
+      for (size_t i = 0; i < m_columns; i++) {
+        delete[] m_matrix[i];
+      }
+
+      delete[] m_matrix;
+
+      m_matrix = nullptr;
+      m_rows = 0;
+      m_columns = 0;
+    }
+
+    return *this;
+  }
+  ~Matrix() {
+    if (m_matrix != nullptr) {
+      // free arrays
+      for (size_t i = 0; i < m_rows; i++) {
+        delete[] m_matrix[i];
+      }
+
+      delete[] m_matrix;
+    }
+    m_matrix = nullptr;
+  }
+  // all operations modify the matrix in-place.
+  void resize(const size_t rows, const size_t columns, const T default_value = 0) {
+    assert(rows > 0 && columns > 0 && "Columns and rows must exist.");
+
+    if (m_matrix == nullptr) {
+      // alloc arrays
+      m_matrix = new T*[rows]; // rows
+      for (size_t i = 0; i < rows; i++) {
+        m_matrix[i] = new T[columns]; // columns
+      }
+
+      m_rows = rows;
+      m_columns = columns;
+      clear();
+    }
+    else {
+      // save array pointer
+      T **new_matrix;
+      // alloc new arrays
+      new_matrix = new T*[rows]; // rows
+      for (size_t i = 0; i < rows; i++) {
+        new_matrix[i] = new T[columns]; // columns
+        for (size_t j = 0; j < columns; j++) {
+          new_matrix[i][j] = default_value;
+        }
+      }
+
+      // copy data from saved pointer to new arrays
+      size_t minrows = (std::min)(rows, m_rows);
+      size_t mincols = (std::min)(columns, m_columns);
+      for (size_t x = 0; x < minrows; x++) {
+        for (size_t y = 0; y < mincols; y++) {
+          new_matrix[x][y] = m_matrix[x][y];
+        }
+      }
+
+      // delete old arrays
+      if (m_matrix != nullptr) {
+        for (size_t i = 0; i < m_rows; i++) {
+          delete[] m_matrix[i];
+        }
+
+        delete[] m_matrix;
+      }
+
+      m_matrix = new_matrix;
+    }
+
+    m_rows = rows;
+    m_columns = columns;
+  }
+  void clear() {
+    assert(m_matrix != nullptr);
+
+    for (size_t i = 0; i < m_rows; i++) {
+      for (size_t j = 0; j < m_columns; j++) {
+        m_matrix[i][j] = 0;
+      }
+    }
+  }
+  T& operator ()(const size_t x, const size_t y) {
+    assert(x < m_rows);
+    assert(y < m_columns);
+    assert(m_matrix != nullptr);
+    return m_matrix[x][y];
+  }
+  const T& operator ()(const size_t x, const size_t y) const {
+    assert(x < m_rows);
+    assert(y < m_columns);
+    assert(m_matrix != nullptr);
+    return m_matrix[x][y];
+  }
+  const T mymin() const {
+    assert(m_matrix != nullptr);
+    assert(m_rows > 0);
+    assert(m_columns > 0);
+    T min = m_matrix[0][0];
+
+    for (size_t i = 0; i < m_rows; i++) {
+      for (size_t j = 0; j < m_columns; j++) {
+        min = std::min<T>(min, m_matrix[i][j]);
+      }
+    }
+
+    return min;
+  }
+  const T mymax() const {
+    assert(m_matrix != nullptr);
+    assert(m_rows > 0);
+    assert(m_columns > 0);
+    T max = m_matrix[0][0];
+
+    for (size_t i = 0; i < m_rows; i++) {
+      for (size_t j = 0; j < m_columns; j++) {
+        max = std::max<T>(max, m_matrix[i][j]);
+      }
+    }
+
+    return max;
+  }
+  inline size_t minsize() { return ((m_rows < m_columns) ? m_rows : m_columns); }
+  inline size_t columns() const { return m_columns; }
+  inline size_t rows() const { return m_rows; }
+
+  friend std::ostream& operator<<(std::ostream& os, const Matrix &matrix)
+  {
+    os << "Matrix:" << std::endl;
+    for (size_t row = 0; row < matrix.rows(); row++)
+    {
+      for (size_t col = 0; col < matrix.columns(); col++)
+      {
+        os.width(8);
+        os << matrix(row, col) << ",";
+      }
+      os << std::endl;
+    }
+    return os;
+  }
+
+private:
+  T **m_matrix;
+  size_t m_rows;
+  size_t m_columns;
+};
+
+
+
+template<typename Data> class Munkres
+{
+  static constexpr int NORMAL = 0;
+  static constexpr int STAR = 1;
+  static constexpr int PRIME = 2;
+public:
+
+  /*
+  *
+  * Linear assignment problem solution
+  * [modifies matrix in-place.]
+  * matrix(row,col): row major format assumed.
+  *
+  * Assignments are remaining 0 values
+  * (extra 0 values are replaced with -1)
+  *
+  */
+  void solve(Matrix<Data> &m) {
+    const size_t rows = m.rows(),
+      columns = m.columns(),
+      size = (std::max)(rows, columns);
+
+#ifdef DEBUG
+    std::cout << "Munkres input: " << m << std::endl;
+#endif
+
+    // Copy input matrix
+    this->matrix = m;
+
+    if (rows != columns) {
+      // If the input matrix isn't square, make it square
+      // and fill the empty values with the largest value present
+      // in the matrix.
+      matrix.resize(size, size, matrix.mymax());
+    }
+
+
+    // STAR == 1 == starred, PRIME == 2 == primed
+    mask_matrix.resize(size, size);
+
+    row_mask = new bool[size];
+    col_mask = new bool[size];
+    for (size_t i = 0; i < size; i++) {
+      row_mask[i] = false;
+    }
+
+    for (size_t i = 0; i < size; i++) {
+      col_mask[i] = false;
+    }
+
+    // Prepare the matrix values...
+
+    // If there were any infinities, replace them with a value greater
+    // than the maximum value in the matrix.
+    replace_infinites(matrix);
+
+    minimize_along_direction(matrix, rows >= columns);
+    minimize_along_direction(matrix, rows <  columns);
+
+    // Follow the steps
+    int step = 1;
+    while (step) {
+      switch (step) {
+      case 1:
+        step = step1();
+        // step is always 2
+        break;
+      case 2:
+        step = step2();
+        // step is always either 0 or 3
+        break;
+      case 3:
+        step = step3();
+        // step in [3, 4, 5]
+        break;
+      case 4:
+        step = step4();
+        // step is always 2
+        break;
+      case 5:
+        step = step5();
+        // step is always 3
+        break;
+      }
+    }
+
+    // Store results
+    for (size_t row = 0; row < size; row++) {
+      for (size_t col = 0; col < size; col++) {
+        if (mask_matrix(row, col) == STAR) {
+          matrix(row, col) = 0;
+        }
+        else {
+          matrix(row, col) = -1;
+        }
+      }
+    }
+
+#ifdef DEBUG
+    std::cout << "Munkres output: " << matrix << std::endl;
+#endif
+    // Remove the excess rows or columns that we added to fit the
+    // input to a square matrix.
+    matrix.resize(rows, columns);
+
+    m = matrix;
+
+    delete[] row_mask;
+    delete[] col_mask;
+  }
+
+  static void replace_infinites(Matrix<Data> &matrix) {
+    const size_t rows = matrix.rows(),
+      columns = matrix.columns();
+    assert(rows > 0 && columns > 0);
+    double max = matrix(0, 0);
+    constexpr auto infinity = std::numeric_limits<double>::infinity();
+
+    // Find the greatest value in the matrix that isn't infinity.
+    for (size_t row = 0; row < rows; row++) {
+      for (size_t col = 0; col < columns; col++) {
+        if (matrix(row, col) != infinity) {
+          if (max == infinity) {
+            max = matrix(row, col);
+          }
+          else {
+            max = std::max<double>(max, matrix(row, col));
+          }
+        }
+      }
+    }
+
+    // a value higher than the maximum value present in the matrix.
+    if (max == infinity) {
+      // This case only occurs when all values are infinite.
+      max = 0;
+    }
+    else {
+      max++;
+    }
+
+    for (size_t row = 0; row < rows; row++) {
+      for (size_t col = 0; col < columns; col++) {
+        if (matrix(row, col) == infinity) {
+          matrix(row, col) = max;
+        }
+      }
+    }
+
+  }
+
+  static void minimize_along_direction(Matrix<Data> &matrix, const bool over_columns) {
+    const size_t outer_size = over_columns ? matrix.columns() : matrix.rows(),
+      inner_size = over_columns ? matrix.rows() : matrix.columns();
+
+    // Look for a minimum value to subtract from all values along
+    // the "outer" direction.
+    for (size_t i = 0; i < outer_size; i++) {
+      double min = over_columns ? matrix(0, i) : matrix(i, 0);
+
+      // As long as the current minimum is greater than zero,
+      // keep looking for the minimum.
+      // Start at one because we already have the 0th value in min.
+      for (size_t j = 1; j < inner_size && min > 0; j++) {
+        min = std::min<double>(
+          min,
+          over_columns ? matrix(j, i) : matrix(i, j));
+      }
+
+      if (min > 0) {
+        for (size_t j = 0; j < inner_size; j++) {
+          if (over_columns) {
+            matrix(j, i) -= min;
+          }
+          else {
+            matrix(i, j) -= min;
+          }
+        }
+      }
+    }
+  }
+
+private:
+
+  inline bool find_uncovered_in_matrix(const double item, size_t &row, size_t &col) const {
+    const size_t rows = matrix.rows(),
+      columns = matrix.columns();
+
+    for (row = 0; row < rows; row++) {
+      if (!row_mask[row]) {
+        for (col = 0; col < columns; col++) {
+          if (!col_mask[col]) {
+            if (matrix(row, col) == item) {
+              return true;
+            }
+          }
+        }
+      }
+    }
+
+    return false;
+  }
+
+  bool pair_in_list(const std::pair<size_t, size_t> &needle, const std::list<std::pair<size_t, size_t> > &haystack) {
+    for (std::list<std::pair<size_t, size_t> >::const_iterator i = haystack.begin(); i != haystack.end(); i++) {
+      if (needle == *i) {
+        return true;
+      }
+    }
+
+    return false;
+  }
+
+  int step1() {
+    const size_t rows = matrix.rows(),
+      columns = matrix.columns();
+
+    for (size_t row = 0; row < rows; row++) {
+      for (size_t col = 0; col < columns; col++) {
+        if (0 == matrix(row, col)) {
+          for (size_t nrow = 0; nrow < row; nrow++)
+            if (STAR == mask_matrix(nrow, col))
+              goto next_column;
+
+          mask_matrix(row, col) = STAR;
+          goto next_row;
+        }
+      next_column:;
+      }
+    next_row:;
+    }
+
+    return 2;
+  }
+
+  int step2() {
+    const size_t rows = matrix.rows(),
+      columns = matrix.columns();
+    size_t covercount = 0;
+
+    for (size_t row = 0; row < rows; row++)
+      for (size_t col = 0; col < columns; col++)
+        if (STAR == mask_matrix(row, col)) {
+          col_mask[col] = true;
+          covercount++;
+        }
+
+    if (covercount >= matrix.minsize()) {
+#ifdef DEBUG
+      std::cout << "Final cover count: " << covercount << std::endl;
+#endif
+      return 0;
+    }
+
+#ifdef DEBUG
+    std::cout << "Munkres matrix has " << covercount << " of " << matrix.minsize() << " Columns covered:" << std::endl;
+    std::cout << matrix << std::endl;
+#endif
+
+
+    return 3;
+  }
+
+  int step3() {
+    /*
+    Main Zero Search
+    1. Find an uncovered Z in the distance matrix and prime it. If no such zero exists, go to Step 5
+    2. If No Z* exists in the row of the Z', go to Step 4.
+    3. If a Z* exists, cover this row and uncover the column of the Z*. Return to Step 3.1 to find a new Z
+    */
+    if (find_uncovered_in_matrix(0, saverow, savecol)) {
+      mask_matrix(saverow, savecol) = PRIME; // prime it.
+    }
+    else {
+      return 5;
+    }
+
+    for (size_t ncol = 0; ncol < matrix.columns(); ncol++) {
+      if (mask_matrix(saverow, ncol) == STAR) {
+        row_mask[saverow] = true; //cover this row and
+        col_mask[ncol] = false; // uncover the column containing the starred zero
+        return 3; // repeat
+      }
+    }
+
+    return 4; // no starred zero in the row containing this primed zero
+  }
+
+  int step4() {
+    const size_t rows = matrix.rows(),
+      columns = matrix.columns();
+
+    // seq contains pairs of row/column values where we have found
+    // either a star or a prime that is part of the ``alternating sequence``.
+    std::list<std::pair<size_t, size_t> > seq;
+    // use saverow, savecol from step 3.
+    std::pair<size_t, size_t> z0(saverow, savecol);
+    seq.insert(seq.end(), z0);
+
+    // We have to find these two pairs:
+    std::pair<size_t, size_t> z1(-1, -1);
+    std::pair<size_t, size_t> z2n(-1, -1);
+
+    size_t row, col = savecol;
+    /*
+    Increment Set of Starred Zeros
+    1. Construct the ``alternating sequence'' of primed and starred zeros:
+    Z0 : Unpaired Z' from Step 4.2
+    Z1 : The Z* in the column of Z0
+    Z[2N] : The Z' in the row of Z[2N-1], if such a zero exists
+    Z[2N+1] : The Z* in the column of Z[2N]
+    The sequence eventually terminates with an unpaired Z' = Z[2N] for some N.
+    */
+    bool madepair;
+    do {
+      madepair = false;
+      for (row = 0; row < rows; row++) {
+        if (mask_matrix(row, col) == STAR) {
+          z1.first = row;
+          z1.second = col;
+          if (pair_in_list(z1, seq)) {
+            continue;
+          }
+
+          madepair = true;
+          seq.insert(seq.end(), z1);
+          break;
+        }
+      }
+
+      if (!madepair)
+        break;
+
+      madepair = false;
+
+      for (col = 0; col < columns; col++) {
+        if (mask_matrix(row, col) == PRIME) {
+          z2n.first = row;
+          z2n.second = col;
+          if (pair_in_list(z2n, seq)) {
+            continue;
+          }
+          madepair = true;
+          seq.insert(seq.end(), z2n);
+          break;
+        }
+      }
+    } while (madepair);
+
+    for (std::list<std::pair<size_t, size_t> >::iterator i = seq.begin();
+      i != seq.end();
+      i++) {
+      // 2. Unstar each starred zero of the sequence.
+      if (mask_matrix(i->first, i->second) == STAR)
+        mask_matrix(i->first, i->second) = NORMAL;
+
+      // 3. Star each primed zero of the sequence,
+      // thus increasing the number of starred zeros by one.
+      if (mask_matrix(i->first, i->second) == PRIME)
+        mask_matrix(i->first, i->second) = STAR;
+    }
+
+    // 4. Erase all primes, uncover all columns and rows,
+    for (size_t row = 0; row < mask_matrix.rows(); row++) {
+      for (size_t col = 0; col < mask_matrix.columns(); col++) {
+        if (mask_matrix(row, col) == PRIME) {
+          mask_matrix(row, col) = NORMAL;
+        }
+      }
+    }
+
+    for (size_t i = 0; i < rows; i++) {
+      row_mask[i] = false;
+    }
+
+    for (size_t i = 0; i < columns; i++) {
+      col_mask[i] = false;
+    }
+
+    // and return to Step 2.
+    return 2;
+  }
+
+  int step5() {
+    const size_t rows = matrix.rows(),
+      columns = matrix.columns();
+    /*
+    New Zero Manufactures
+    1. Let h be the smallest uncovered entry in the (modified) distance matrix.
+    2. Add h to all covered rows.
+    3. Subtract h from all uncovered columns
+    4. Return to Step 3, without altering stars, primes, or covers.
+    */
+    double h = std::numeric_limits<double>::max();
+    for (size_t row = 0; row < rows; row++) {
+      if (!row_mask[row]) {
+        for (size_t col = 0; col < columns; col++) {
+          if (!col_mask[col]) {
+            if (h > matrix(row, col) && matrix(row, col) != 0) {
+              h = matrix(row, col);
+            }
+          }
+        }
+      }
+    }
+
+    for (size_t row = 0; row < rows; row++) {
+      if (row_mask[row]) {
+        for (size_t col = 0; col < columns; col++) {
+          matrix(row, col) += h;
+        }
+      }
+    }
+
+    for (size_t col = 0; col < columns; col++) {
+      if (!col_mask[col]) {
+        for (size_t row = 0; row < rows; row++) {
+          matrix(row, col) -= h;
+        }
+      }
+    }
+
+    return 3;
+  }
+
+  Matrix<int> mask_matrix;
+  Matrix<Data> matrix;
+  bool *row_mask;
+  bool *col_mask;
+  size_t saverow = 0, savecol = 0;
+};
+
+
+
+#endif //HUNGARIAN_H
--- Anomalies/src/Support2.cpp
+++ Anomalies/src/Support2.cpp
@@ -8,7 +8,239 @@ bool testLevel3(  string & set_file, short frm_step,
                   list<Observed>    &graphs,
                   set<int>          &obs,
                   int) {
+  
+  FileStorage fs(set_file, FileStorage::READ);
+//..............................................................................
+  string  directory,
+          token,
+          gt_dir,
+          out_matching;
+  
+  fs["load_graph_token"] >> token;
+  fs["load_graph_directory"] >> directory;
+  fs["out_matching_dir"] >> out_matching;
+  fs["groundtruth_dir"] >> gt_dir;
 
+  cutil_file_cont graph_files;
+  list<Observed>  graphs_training;
+
+  cout << "Loading training graphs...\n";
+  list_files(graph_files, directory.c_str(), token.c_str());
+  for (auto & file : graph_files) {
+    cout << file << endl;
+    loadDescribedGraphs(file, graphs_training, &obs);
+  }
+
+//..............................................................................
+  for (auto graph : graphs) {
+    matchNodes(graph, graphs_training, out_matching);
+  }
 
   return true;
+}
+
+/////////////////////////////////////////////////////////////////////////////////
+double matchNodes(Observed &graph_test, list<Observed> &graphs_training, string out_matching) {
+//  graph_test.graph_ = resume(graph_test);
+
+  double match_cost = 0.0, matchs = 0.0, distance = 0.0;
+  int adj = 0, i = 0, Anom = 0, aux = 0, use = 0;
+
+  fstream outFile;
+  outFile.open(out_matching, ios::out | ios::app);  
+  outFile << "Graph " << graph_test.id_ << endl;
+
+  for (auto graph_train : graphs_training) {
+  //  graph_train.graph_ = resume(graph_train);
+    int linha = 0;
+
+    Matrix<double> costMatrix(graph_train.graph_.listNodes_.size(),
+                              graph_test.graph_.listNodes_.size());
+
+    Munkres<double> Hung;
+
+    for (auto node_train : graph_train.graph_.listNodes_) {
+      int coluna = 0;
+      for (auto node_test : graph_test.graph_.listNodes_) {
+        match_cost = 0;
+        if (node_train.objectList_.size() != node_test.objectList_.size())
+          match_cost += 1;
+        /*for (auto obj_train : node_train.objectList_) {
+          auto iter = find(node_test.objectList_.begin(), node_test.objectList_.end(),
+            obj_train);
+          if (iter == node_test.objectList_.end())
+            match_cost += 1;
+          else {
+            double Edge;
+            if (obj_train.second > iter->second)
+              match_cost = abs(obj_train.second - iter->second) / obj_train.second;
+            else
+              match_cost = abs(obj_train.second - iter->second) / iter->second;
+          }
+        }
+        for (auto obj_test : node_test.objectList_) {
+          auto iter = find(node_train.objectList_.begin(), node_train.objectList_.end(),
+            obj_test);
+          if (iter == node_train.objectList_.end())
+            match_cost += 1;
+        }*/
+//-----------------------------------------------------------------------------
+        for (auto obj_train : node_train.objectList_) {
+          for (auto obj_test : node_test.objectList_) {
+            if (obj_train.first.data_.id_ == obj_test.first.data_.id_) {
+              adj = 1;
+              if (obj_train.second > obj_test.second)
+                if(obj_train.second == 0)
+                  match_cost += abs(obj_train.second - obj_test.second)/1;
+                else
+                  match_cost += abs(obj_train.second - obj_test.second) / obj_train.second;
+              else
+                if(obj_test.second == 0)
+                  match_cost += abs(obj_test.second - obj_train.second) / 1;
+                else
+                  match_cost += abs(obj_test.second - obj_train.second)/ obj_test.second;
+            }
+          }
+          if (adj == 0)
+            match_cost += 1;
+          adj = 0;
+        }
+        for (auto obj_test : node_test.objectList_) {
+          for (auto obj_train : node_train.objectList_) {
+            if (obj_train.first.data_.id_ == obj_test.first.data_.id_)
+              adj = 1;
+          }
+          if (adj == 0)
+            match_cost += 1;
+          adj = 0;
+        }
+//------------------------------------------------------------------------------        
+        costMatrix(linha, coluna) = match_cost;
+        coluna++;
+        cout << endl << endl;
+      }
+      linha++;
+    }
+    i++;
+    distance = getDistance(costMatrix, use, graph_train.graph_.listNodes_.size(),
+                           graph_test.graph_.listNodes_.size(), matchs);
+    outFile << "Distance Test Graph " << graph_test.id_ << " to Graph Training "
+            << i << " = " << distance << endl;
+    if (Anom > distance)
+      Anom = distance;
+  }
+  if (Anom > 1) {}
+    
+  outFile << endl << endl;
+  return 1.0;
+}
+
+////////////////////////////////////////////////////////////////////////////////
+double getDistance(Matrix<double> costMatrix, 
+                   int &use,
+                   int numNodesTr, 
+                   int numNodesTes, 
+                   double &matchs) {
+  
+  Munkres<double> Hungarian;
+  int parts = 0;
+  double cost = 0.0, percent = 0.0, dist = 0.0, dist1 = 0.0, dist2 = 0.0, eq = 0,
+         eq1 = 0;
+  matchs = 0.0; use = 0;;
+  if (numNodesTr > numNodesTes) {
+    Matrix<double> permMatrix(numNodesTr, numNodesTes);
+    for (int i = 0; i < numNodesTr; i++) {
+      for (int j = 0; j < numNodesTes; j++) {
+        permMatrix(i, j) = costMatrix(i, j);
+      }
+    }
+    Hungarian.solve(permMatrix);
+    for (int i = 0; i < numNodesTr; i++) {
+      for (int j = 0; j < numNodesTes; j++) {
+        if (permMatrix(i, j) == 0) {
+          cost += costMatrix(i, j);
+          use += 1;
+          if (costMatrix(i, j) < 1) {
+            matchs += 1.0;
+          }
+          break;
+        }
+      }
+    }
+    percent = 1 - (matchs / numNodesTes);
+    dist = (cost / numNodesTes) + (numNodesTr - numNodesTes) * percent;
+  }
+  else {
+    int loop;
+    while (parts < numNodesTes) {
+      cost = 0; 
+      double match = 0;
+      if ((parts + numNodesTr) > numNodesTes) {
+        loop = parts;
+        parts = numNodesTes;
+      }
+      else {
+        loop = parts;
+        parts += numNodesTr;
+      }
+      Matrix<double> permMatrix(numNodesTr, (parts - loop));
+      for (int i = 0; i < numNodesTr; i++) {
+        for (int j = loop; j < parts; j++) {
+          permMatrix(i, (j - loop)) = costMatrix(i, j);
+        }
+      }
+      Hungarian.solve(permMatrix);
+      for (int i = 0; i < numNodesTr; i++) {
+        for (int j = loop; j < parts; j++) {
+          if (permMatrix(i, (j - loop)) == 0) {
+            cost += costMatrix(i, j);
+            use += 1;
+            if (costMatrix(i, j) < 1) {
+              match += 1.0;
+            }
+            break;
+          }
+        }
+      }
+      eq = match / (parts - loop);
+      if (eq > 0) {
+        percent = 1 - (match / (parts - loop));
+        dist1 += (cost / (parts - loop)) + (numNodesTr - (parts - loop)) * percent;
+        eq1 += eq;
+      }
+      else 
+        dist2 += (cost / (parts - loop)) + (numNodesTr - (parts - loop));
+      matchs += match;
+    }
+    dist += (dist1 / (1 + eq1)) + dist2;
+  }
+  return dist;
+}
+
+////////////////////////////////////////////////////////////////////////////////
+graphType resume(Observed & src) {
+
+  graphType newg;
+
+  auto it = src.graph_.listNodes_.begin();
+  set<int> objects;
+  for (auto &par : it->objectList_) {
+    objects.insert(par.first.data_.id_);
+      }
+    auto  str = set2str(objects);
+
+    newg.listNodes_.push_back(*it);
+    it++;
+    for (; it != src.graph_.listNodes_.end(); ++it) {
+      objects.clear();
+      for (auto &par : it->objectList_) {
+        objects.insert(par.first.data_.id_);
+      }
+      auto  str2 = set2str(objects);
+      if (str != str2) {
+        newg.listNodes_.push_back(*it);
+      }
+      str = str2;
+  }
+    return newg;
 }
